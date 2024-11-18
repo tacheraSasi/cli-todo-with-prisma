0b392dc40354eadc36e5e7e1637a2ddf4721e93d
tacheraSasi <tacherasasi@gmail.com>
2024-11-18 19:01:26 +0300

    add todo func

--- todos.go
+++ todos.go
@@ -3,12 +3,19 @@ package main
 import (
 	"cli-todo/db"
 	"context"
+	"fmt"
+	"log"
 )
 
 func AddTodo(client *db.PrismaClient,title string)error{
 	ctx := context.Background()
 
-	addedTodo, err := client.Todo.CreateOne(
-		db
-	)
+	addedTodo,err := client.Todo.CreateOne(db.Todo.Title.Set(title)).Exec(ctx)
+	if err != nil{
+		log.Fatal("Failed to create the todo",err);
+		return err
+	}
+
+	fmt.Println("Todo was created")
+	fmt.Println(addedTodo)
 }
